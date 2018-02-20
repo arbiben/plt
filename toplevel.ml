2e88785f736d21cec3330fb129e709e78a9447a2
OriAboodi <OriAboodi@users.noreply.github.com>
2018-02-20 02:26:00 +0000

    fixed AST problem
    Signed-off-by: OriAboodi <OriAboodi@users.noreply.github.com>

--- toplevel.ml
+++ toplevel.ml
@@ -233,4 +233,4 @@ let () =
   Arg.parse [] (fun file -> channel := open_in file) usage_msg;
   let lexbuf = Lexing.from_channel !channel in
   let ast = Parser.program Scanner.token lexbuf in
-  print_string (AST.string_of_program ast)
+  print_string (Ast.string_of_program ast)
