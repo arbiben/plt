1759aa659cc2efe8853e1fdfd68664e946fc7e81
TomSwifter <t.aharoni@columbia.edu>
2018-02-21 20:22:03 +0000

    modified runprogram.py to only run on the number of files in the dir

--- runprogram.py
+++ runprogram.py
@@ -4,7 +4,12 @@
 import os
 import subprocess
 
-for i in range(1,6):
+#this should always be 10 -- geting the number of files in the dir
+DIR = './tests'
+length = len([name for name in os.listdir(DIR) if name.endswith('.fi') and os.path.isfile(os.path.join(DIR, name))])
+
+#half of the files are positive tests
+for i in range(1,(length/2)+1):
 	command = "./toplevel.native ./tests/postest{}.fi > test{}.out".format(i,i)
 	os.system(command)
 	v = open('test{}.out'.format(i), 'rb').read() == open('./tests/postest{}.out'.format(i), 'rb').read()
@@ -12,8 +17,8 @@ for i in range(1,6):
 	command = "rm ./test{}.out".format(i)
 	os.system(command)
 
-
-for i in range(1,6):
+#half of the files are negative tests
+for i in range(1,(length/2)+1):
 	command = "./toplevel.native ./tests/negtest{}.fi".format(i)
 	error = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 	out, error = error.communicate()
