f75b85b313cda2d59fa89f10c9ab9c8f0cca20d4
gh2434 <gh2434@columbia.edu>
2018-03-30 15:01:13 +0000

    finished loops in codegen besides adding a return if last block falls off the end

--- codegen.ml
+++ codegen.ml
@@ -13,7 +13,7 @@ module StringMap = Map.Make(String)
 
 (* Code Generation from the SAST. Returns an LLVM module if successful,
    throws an exception if something is wrong. *)
-let translate ((_, functions), _) =
+let translate ((globals, functions), _) =
   let context    = L.global_context () in
   (* Add types to the context so we can use them in our LLVM code *)
   let i32_t      = L.i32_type                context
@@ -28,55 +28,194 @@ let translate ((_, functions), _) =
       A.Int   -> i32_t
     | A.Str   -> ptr
     | A.Bool  -> i1_t 
-    | t -> raise (Failure (A.string_of_typ t ^ "not implemented yet"))
+   (* | t -> raise (Failure (A.string_of_typ t ^ "not implemented yet"))*)
   in
 
+  let global_vars = 
+      let global_var m (t, n) =
+          let init = L.const_int (ltype_of_typ t) 0
+          in StringMap.add n (L.define_global n init the_module) m in
+        List.fold_left global_var StringMap.empty globals in 
   (* Declare a printf function to implement print. *)
   let printf_t : L.lltype = 
       L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func : L.llvalue = 
      L.declare_function "printf" printf_t the_module in 
 
-  let to_imp str = raise (Failure ("Not yet implemented: " ^ str)) in
+  let function_decls = 
+        let function_decl m fdecl = 
+            let name = fdecl.sfname
+            and formal_types = 
+                Array.of_list (List.map (fun (t, _) -> ltype_of_typ t) fdecl.sformals)
+               in let ftype = L.function_type (ltype_of_typ fdecl.styp) formal_types in 
+               StringMap.add name (L.define_function name ftype the_module, fdecl) m in
+             List.fold_left function_decl StringMap.empty functions in
 
   (* Generate the instructions for a trivial main function *)
-  let build_function fdecl =
+  let build_function_body fdecl =
+    let (the_function, _) = StringMap.find fdecl.sfname function_decls in
     (* Make the LLVM module aware of the main function *)
+    (* CAN ERASEEEEE
     let main_ty = L.function_type (ltype_of_typ fdecl.styp) [||] in
     let the_function = L.define_function "main" main_ty the_module in
+    *)
     (* Create an Instruction Builder, which points into a basic block
       and determines where the next instruction should be placed *)
     let builder = L.builder_at_end context (L.entry_block the_function) in
+    
     (* Create a pointer to a format string for printf *)
     let int_format_str = L.build_global_stringptr "%d" "fmt" builder in
     let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in 
+    
+    let local_vars = 
+        let add_formal m (t, n) p =
+            let () = L.set_value_name n p in
+            let local = L.build_alloca (ltype_of_typ t) n builder in
+            let _ = L.build_store p local builder in
+            StringMap.add n local m
+        in
+
+        let add_local m (t, n) = 
+            let local_var = L.build_alloca (ltype_of_typ t) n builder
+            in StringMap.add n local_var m
+        in 
+
+        let formals = List.fold_left2 add_formal StringMap.empty fdecl.sformals
+              (Array.to_list (L.params the_function)) in
+        List.fold_left add_local formals fdecl.slocals
+    in 
+    let lookup n = try StringMap.find n local_vars with Not_found ->
+                        StringMap.find n global_vars
+    in 
+
     (* Generate LLVM code for a call to print *)
     let rec expr builder ((_, e) : sexpr) = match e with
-      SLiteral i -> L.const_int i32_t i (* Generate a constant integer *)
+        SLiteral i -> L.const_int i32_t i (* Generate a constant integer *)
       | SBoolLit b -> L.const_int i1_t (if b then 1 else 0) 
       | SStrLit i -> L.build_global_stringptr i "tmp" builder (* generate a pointer *)
+      | SNoexpr -> L.const_int i32_t 0
+      | SId s -> L.build_load (lookup s) s builder
+      | SBinop (e1, op, e2) ->
+              let (t, _) = e1
+              and e1' = expr builder e1
+              and e2' = expr builder e2 in
+              (match op with
+                A.Add     -> L.build_add
+              | A.Sub     -> L.build_sub
+              | A.Mult    -> L.build_mul
+              | A.Div     -> L.build_sdiv
+              | A.And     -> L.build_and
+              | A.Or      -> L.build_or
+              | A.Mod     -> L.build_srem
+              | A.Equal   -> L.build_icmp L.Icmp.Eq
+              | A.Neq     -> L.build_icmp L.Icmp.Ne
+              | A.Less    -> L.build_icmp L.Icmp.Slt
+              | A.Leq     -> L.build_icmp L.Icmp.Sle
+              | A.Greater -> L.build_icmp L.Icmp.Sgt
+              | A.Geq     -> L.build_icmp L.Icmp.Sge
+              ) e1' e2' "tmp" builder
+      | SUnop(op, e) -> 
+              let (t, _) = e and e' = expr builder e in
+              (match op with
+                A.Neg     -> L.build_neg
+              | A.Not     -> L.build_not) e' "tmp" builder
+      | SAssign (s, e)    -> let e' = expr builder e in
+                             let _  = L.build_store e' (lookup s) builder in e' 
       | SCall ("print", [e]) -> (* Generate a call instruction *)
-  L.build_call printf_func [| int_format_str ; (expr builder e) |]
-    "printf" builder 
+                        L.build_call printf_func [| int_format_str ; (expr builder e) |]
+                                "printf" builder 
       | SCall ("printstring", [e]) ->  
   L.build_call printf_func [| str_format_str ; (expr builder e) |] "printf" builder
       (* Throw an error for any other expressions *)
-      | _ -> to_imp (string_of_sexpr (A.Int,e))  
-    in
+      | SCall (f, act) -> 
+              let (fdef, fdecl) = StringMap.find f function_decls in
+              let actuals = List.rev (List.map (expr builder) (List.rev act)) in
+              let result = f ^ "_result" in
+              L.build_call fdef (Array.of_list actuals) result builder in
+
+              
+   
+
+   (* Each basic block in a program ends with a "terminator" instruction i.e.
+    *     one that ends the basic block. By definition, these instructions must
+    *         indicate which basic block comes next -- they typically yield "void" value
+    *             and produce control flow, not values *)
+       (* Invoke "f builder" if the current block doesn't already
+        *        have a terminator (e.g., a branch). *)
+     let add_terminal builder f =
+     (* The current block where we're inserting instr *)
+         match L.block_terminator (L.insertion_block builder) with
+         Some _ -> ()
+       | None -> ignore (f builder) in
     (* Deal with a block of expression statements, terminated by a return *)
     let rec stmt builder = function
-SBlock sl -> List.fold_left stmt builder sl
+        SBlock sl -> List.fold_left stmt builder sl
       | SExpr e -> let _ = expr builder e in builder 
-      | SReturn e -> let _ = match fdecl.styp with
-                              A.Int -> L.build_ret (expr builder e) builder 
-                            | _ -> to_imp (A.string_of_typ fdecl.styp)
-                     in builder
-      | s -> to_imp (string_of_sstmt s)
+      | SReturn e -> let _ = L.build_ret (expr builder e) builder in builder
     (* Generate the instructions for the function's body, 
        which mutates the_module *)
-    in ignore(stmt builder (SBlock fdecl.sbody))
+     | SIf (predicate, then_stmt, else_stmt) ->
+       let bool_val = expr builder predicate in
+       (* Add "merge" basic block to our function's list of blocks *)
+       let merge_bb = L.append_block context "merge" the_function in
+       (* Partial function used to generate branch to merge block *) 
+       let branch_instr = L.build_br merge_bb in
+       (* Same for "then" basic block *)
+       let then_bb = L.append_block context "then" the_function in
+       (* Position builder in "then" block and build the statement *)
+       let then_builder = stmt (L.builder_at_end context then_bb) then_stmt in
+       (* Add a branch to the "then" block (to the merge block) 
+      * if a terminator doesn't already exist for the "then" block *)
+       let () = add_terminal then_builder branch_instr in
+       (* Identical to stuff we did for "then" *)
+       let else_bb = L.append_block context "else" the_function in
+       let else_builder = stmt (L.builder_at_end context else_bb) else_stmt in
+       let () = add_terminal else_builder branch_instr in
+       (* Generate initial branch instruction perform the selection of "then"
+        * or "else". Note we're using the builder we had access to at the start
+        * of this alternative. *)
+       let _ = L.build_cond_br bool_val then_bb else_bb builder in
+       (* Move to the merge block for further instruction building *)
+       L.builder_at_end context merge_bb
+       | SWhile (predicate, body) ->
+       (* First create basic block for condition instructions -- this will
+        * serve as destination in the case of a loop *)
+       let pred_bb = L.append_block context "while" the_function in
+       (* In current block, branch to predicate to execute the condition *)
+       let _ = L.build_br pred_bb builder in
+      (* Create the body's block, generate the code for it, and add a branch
+       * back to the predicate block (we always jump back at the end of a while
+       * loop's body, unless we returned or something) *)
+       let body_bb = L.append_block context "while_body" the_function in
+              let while_builder = stmt (L.builder_at_end context body_bb) body in
+       let () = add_terminal while_builder (L.build_br pred_bb) in
+   
+       (* Generate the predicate code in the predicate block *)
+       let pred_builder = L.builder_at_end context pred_bb in
+       let bool_val = expr pred_builder predicate in
+
+       (* Hook everything up *)
+       let merge_bb = L.append_block context "merge" the_function in
+       let _ = L.build_cond_br bool_val body_bb merge_bb pred_builder in
+       L.builder_at_end context merge_bb
+       
+       (* Implement for loops as while loops! *)
+       | SFor (e1, e2, e3, body) -> stmt builder
+       ( SBlock [SExpr e1 ; SWhile (e2, SBlock [body ; SExpr e3]) ] )
+         in
+
+       let builder = stmt builder (SBlock fdecl.sbody) in
+       (* Add a return if the last block falls off the end *)
+        add_terminal builder fdecl.styp =  
+              build_ret (L.const_int (ltype_of_typ fdecl.styp) 0)
+        
+          
+  
+
+
+   (* in ignore(stmt builder (SBlock fdecl.sbody))*)
   (* Build each function (there should only be one for Hello World), 
      and return the final module *)
-  in List.iter build_function functions;
-the_module
+  in List.iter build_function_body functions;
+ the_module
 
