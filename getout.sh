ece93c323e9dc5f102254549d85c6acafd24afa0
TomSwifter <t.aharoni@columbia.edu>
2018-05-08 01:13:19 +0000

    made an amazing testing script

--- getout.sh
+++ getout.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 dir="~/plt/tests/*"
-for file in ~/plt/tests/*.out
+for file in ~/plt/tests/*.fi
 do
     filebname="$(basename $file)"
     if [ -f "$file" ]
     then
-        echo "$filebname:" 
-        echo "Expected Output:"
-        cat "$file"
-        echo "
-        "
-        echo "---------------------------------------------------------------"
-    fi
-done
+        echo "File: $filebname" >> files.doc
+        cat "$file" >> files.doc
+        echo "---------------------------------------------------------------" >> files.doc
+        echo "Expected Output:" >> files.doc
+        tmp1=$(echo "$file" | sed 's/.fi/.out/')
+        tmp2=$(echo "$file" | sed 's/.fi/.err/')
+        cat $tmp1 >> files.doc 2>/dev/null
+        cat $tmp2 >> files.doc 2>/dev/null
 
-for file in ~/plt/tests/*.err
-do
-    filebname="$(basename $file)"
-    if [ -f "$file" ]
-    then
-        echo "$filebname:" 
-        echo "Expected Output:"
-        cat "$file"
-        echo "
-        "
-        echo "---------------------------------------------------------------"
+        echo "---------------------------------------------------------------" >> files.doc
     fi
 done
 
